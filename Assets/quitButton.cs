3c4fa6ede0d148b14c662e77d04b81eb55e58281
スキルアップ用 <daiki.ph.fx@gmail.com>
2020-06-27 09:25:56 +0900

    Scene adjusted

---
+++ Assets/quitButton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quitButton : MonoBehaviour
+{
+
+    //　ゲーム終了ボタンを押したら実行する
+  public void EndGame() {
+  	#if UNITY_EDITOR
+  		UnityEditor.EditorApplication.isPlaying = false;
+  	#else
+  		Application.Quit();
+  	#endif
+  }
+
+  public void OnClick()
+  {
+    EndGame();
+  }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
