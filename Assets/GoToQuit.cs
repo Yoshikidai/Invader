ec1091d05c5f83d421ac3192dd65b45cd12a4d98
スキルアップ用 <daiki.ph.fx@gmail.com>
2020-07-06 23:48:10 +0900

    Edited quitButton

---
+++ Assets/GoToQuit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoToQuit : MonoBehaviour
+{
+  public GameObject quitPanel;
+  public GameObject unablePanel;
+
+  public void OnClick()
+  {
+    quitPanel.SetActive(true);
+    unablePanel.SetActive(true);
+
+  }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/cancelQuit.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cancelQuit : MonoBehaviour
+{
+  public GameObject quitPanel;
+  public GameObject unablePanel;
+
+  public void OnClick()
+  {
+    unablePanel.SetActive(false);
+    quitPanel.SetActive(false);
+
+  }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
